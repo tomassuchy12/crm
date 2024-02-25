21a18196970f40be7bb11397a443b505c4e48c6d
Tomáš Suchý <tomas.suchy@vivantis.cz>
2024-02-25 14:51:29 +0100

    added navigation

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>
-    <HelloWorld msg="Hi" />
+  <div class="border-b-2">
+    <div class="h-fit w-fit p-4">
+    <RouterLink to="/">
+      <picture>
+        <source srcset="./assets/vue.svg" />
+        <img src="./assets/vue.svg">
+      </picture>
+    </RouterLink>
+    </div>
+  </div>
+  <div class="h-[calc(100vh-70px)] flex">
+    <div class="border-r-2 p-4">
+      <ul>
+        <RouterLink to="/users">
+          <li>
+            Users
+          </li>
+        </RouterLink>
+      </ul>
+    </div>
+    <div class="p-4 w-full">
+      <RouterView />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import HelloWorld from '@components/HelloWorld.vue';
 </script>
